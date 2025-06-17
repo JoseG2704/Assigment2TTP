59273f20a4bdaa3f2fdae7f64b8f71307f29c0d6
Tran Vo <vobaohoangtran@gmail.com>
2025-06-17 14:51:05 -0400

    up to every()

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// forEach() //
+// #1 forEach() //
 function myEach(arr) {
   for (let i = 0; i < arr.length; i++) {
     console.log(arr[i]);
@@ -13,7 +13,7 @@ myEach(numbers); // Output: 1 2 3 4 5
 
 
 
-// map() //
+// #2 map() //
 function myMap(arr, callback) {
   let result = [];
   for (let i = 0; i < arr.length; i++) {
@@ -33,7 +33,7 @@ let doubledNumbers = myMap(numbers2, function(number) {
 
 console.log(doubledNumbers)
 
-// filter() //
+// #3 filter() //
 function myFilter(arr) {
   let evenNum = [];
   for (let i = 0; i < arr.length; i++) {
@@ -48,3 +48,34 @@ problem3 = "#3 filter(), myFilter()";
 console.log(problem3);
 let filteredNumbers = myFilter([1, 2, 3, 4, 5]);
 console.log(filteredNumbers); 
+
+// #4 some() //
+function mySome(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      return true;
+    }
+  }
+  return false;
+}
+//test
+problem4 = "#4 some(), mySome()";
+console.log(problem4);
+let hasEvenNumber = mySome([1, 3, 5, 7, 8]);
+console.log(hasEvenNumber); 
+
+// every() //
+function myEvery(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 !== 0) {
+      return false; //if ood is found
+    }
+  }
+  return true;//all even
+}
+//test
+problem5 = "#5 every(), myEvery()";
+console.log(problem5);
+let allEven = myEvery([2, 4, 6, 8]);
+console.log(allEven); 
+
